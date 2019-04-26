af0e1d84754372dc928dfad098a798dd1f126acd
Gabbo16 <gabri.bressan@gmail.com>
2019-04-26 18:44:09 +0200

    Added SplashScreen

---
+++ app/src/main/java/com/example/gabri/thecalendar/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.gabri.thecalendar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gabri on 26/04/19.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainPage.class));
+        finish();
+    }
+}
